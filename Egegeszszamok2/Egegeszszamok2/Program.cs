9d8ba28763b0911ed7e11487c99edb7ee9755847
Balatoni Dávid <baldav0912@gmail.com>
2018-12-06 11:10:34 +0100

    Adatmegjelenítés megvalósítva

--- Egegeszszamok2/Egegeszszamok2/Program.cs
+++ Egegeszszamok2/Egegeszszamok2/Program.cs
@@ -11,9 +11,11 @@ namespace Egegeszszamok2
         static void Main(string[] args)
         {
             int a=0;
+            Console.WriteLine("a=" + a);
             int b=1;
-            
-            
+            Console.WriteLine("b=" + b);
+            Console.ReadKey();
+
         }
     }
 }
