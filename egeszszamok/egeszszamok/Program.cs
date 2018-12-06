ee95ce8275e5325d14f8c571096e9fe4e5c27f06
Balatoni Dávid <baldav0912@gmail.com>
2018-12-06 10:07:26 +0100

    project létrejött

---
+++ egeszszamok/egeszszamok/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace egeszszamok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
